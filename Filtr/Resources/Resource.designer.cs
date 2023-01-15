4c696a3cadcf0548ec00316bece14eaa43e77f6e
Amir Aharon <amir.the.junior@gmail.com>
2023-01-15 19:43:10 +0200

    Titles onclick DONE!

--- Filtr/Resources/Resource.designer.cs
+++ Filtr/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace Filtr
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.1.0.5")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.2.0.155")]
 	public partial class Resource
 	{
 		
